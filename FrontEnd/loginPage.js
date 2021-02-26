6989e51a1e74092cb843ec7667653e33eb9ac5a6
Kwcunnin <44002642+Kwcunnin@users.noreply.github.com>
2021-02-26 00:33:24 -0600

    Add files via upload

---
+++ FrontEnd/loginPage.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("LoginForm");
+const loginButton = document.getElementById("submitButton");
+
+
+loginButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	const username = loginForm.username.value;
+	const password = loginForm.password.value;
+	
+	//placeholder for username that will be held in server
+	if (username === "username1" && password === "password1"){
+		//TO DO check if mandatory profile things are blank if so take them to the create profile page
+		window.location="editProfile.html"
+	}
+	else {
+		//TO DO CHANGE FROM ALERT TO POP UP
+		alert("Wrong password try again");
+	}
+})
+
