bf58bd1cc7008951645ea9c880a7fadedd11ea8a
Kwcunnin <44002642+Kwcunnin@users.noreply.github.com>
2021-03-20 18:04:05 -0500

    Update loginModule.php

--- BackEnd/loginModule.php
+++ BackEnd/loginModule.php
@@ -5,14 +5,10 @@ class loginMod{
 	public function  login(){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-		if ($this->validUsername($username)){
-			if ($this->correctCredentials($username,$password)){
-				$_SESSION['username'] = $username;
-				$url = '//localhost/home.html';
-				header("Location: $url");
-			}else {
-				echo "<script>alert('Wrong credentials try again')</script>";//Invalid Login
-			}
+		if ($this->validUsername($username) && $this->correctCredentials($username,$password)){
+			$_SESSION['username'] = $username;
+			$url = '//localhost/home.html';
+			header("Location: $url");
 		}
 		else {
 			echo "<script>alert('Wrong credentials try again')</script>";//Invalid Login
@@ -23,19 +19,14 @@ class loginMod{
                 'username1'=>'password1',
                 'username2'=>'password2'
                 );
-		if (array_key_exists($username,$userinfo)){
-			return true;
-		}
-		else{
-			return false;
-		}
+		return (array_key_exists($username,$userinfo));
 	}
 	public function correctCredentials($username, $password): bool {
 		$userinfo = array(
                 'username1'=>'password1',
                 'username2'=>'password2'
                 );
-		return true;
+		return ($userinfo[$username] == $password);
 	}
 }	
 ?>
