a0f6d328a34ce7c0812bfbc6177cc5da86919759
Kwcunnin <44002642+Kwcunnin@users.noreply.github.com>
2021-03-21 00:19:33 -0500

    Update and rename testLoginModule.php to testAllModule.php

---
+++ TestCases/testAllModule.php
@@ -0,0 +1,61 @@
+
+<?php
+include: loginModule.php;
+include: editProfile.php;
+include: fuelQuoteHistModule.php;
+include: fuelQuoteModule.php;
+include: registration.php;
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+	public function testWrongUserName(){
+		$loginObj = new loginMod;
+		$this->assertFalse($loginObj->validUsername('incorrect'));
+	}
+	public function testValidUsername(){
+		$loginObj = new loginMod;
+		$this->assertTrue($loginObj->validUsername('username1'));
+	}
+	public function testCorrectCredentials(){
+		$loginObj = new loginMod;
+		$this->assertTrue($loginObj->correctCredentials('username1','password1'));
+	}
+	public function testIncorrectCredentials(){
+		$loginObj = new loginMod;
+		$this->assertFalse($loginObj->(correctCredentials('username1','incorrect'));
+	}
+	public function testLogin(){
+		$loginObj = new loginMod;
+		$this->assertTrue($loginObj->login('username1','password1');
+		$this->assertFalse($loginObj->login('wrong','wrong');
+	}
+	public function testEditProfile(){
+		$editObj = new 	editProfMod;
+		$this->assertTrue($editObj->edit('name','address1','address2','city','state','12345');
+		$this->assertFalse($editObj->edit('name!!','address1','address2','city','state','12345');
+		$this->assertFalse($editObj->edit('name','address1','address2','city!!!','state','12345');
+		$this->assertFalse($editObj->edit('name','address1','address2','city','state!!!','12345');
+		$this->assertFalse($editObj->edit('name','address1','address2','city','state','12345dasdasd');
+		$this->assertFalse($editObj->edit(null,'address1','address2','city','state','12345');
+		}
+	public function testFuelQuoteHistory(){
+		$histObj = new fuelQuoteHistMod;
+		$this->assertTrue($histObj->hist('text.txt');
+		$this->assertFalse($histObj->hist('notAFile.txt');
+	}
+	public function testFuelQuote(){
+		$quoteObj = new fuelQuoteMod;
+		$this->assertTrue($quoteObj->quote('6 gallons','02/12/2022');
+		$this->assertFalse($quoteObj->quote(null,null);
+	}
+	public function testRegister(){
+		$regObj = new registrationMod;
+		$this->assertFalse('username1','password');
+		$this->assertFalse('username3','2shrt');
+		$this->assertTrue('username3','password3');
+	}
+
+}
+
+?>
