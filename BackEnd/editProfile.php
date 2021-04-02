96c16adfe440e86a60f07d6337ebb61085c3ba9e
adalrymple <75194086+adalrymple@users.noreply.github.com>
2021-04-02 14:54:03 -0500

    Add files via upload

--- BackEnd/editProfile.php
+++ BackEnd/editProfile.php
@@ -1,47 +1,7 @@
 <?php
     session_start();
     class eProfile{
-        public function profile($name = NULL, $address1 = NULL, $address2 = NULL, $city = NULL, $state = NULL, $zipcode = NULL){
-            $db = mysqli_connect('localhost', 'root', 'your_password', 'fuel_thing');
-            if(!$db){
-                die('Error connectinf to the sql server' . mysqli_connect_error());
-            }
-            if($name == NULL && ctype_alpha($_POST['name']) && strlen($_POST['name']) <= 50){
-                $name = $_POST['name'];
-            }
-            if($address1 == NULL && strlen($_POST['address1']) <= 100){
-                $address1 = $_POST['address1'];
-            }
-            if($address2 == NULL && isset($_POST['address2']) && strlen($_POST['address2']) <= 100){
-                $address2 = $_POST['address2'];
-            }
-            if($city == NULL && ctype_alpha($_POST['city']) && strlen($_POST['city']) <= 100){
-                $city = $_POST['city'];
-            }
-            if($state == NULL && ctype_alpha($_POST['state']) && strlen($_POST['name']) == 2){
-                $state = $_POST['state'];
-            }
-            $pattern = '/\d{5,9}/';
-            if($zipcode == NULL && preg_match($pattern,$_POST['zipcode'])){
-                $zipcode = $_POST['zipcode'];
-            }
-
-            if($name && $address1 && $city && $state && $zipcode){
-                if(isCreated($_SESSION['username'], $db)){
-                    $sql = "UPDATE clientInformation 
-                    SET client_name = '$name', address1 = '$address1', address2 = '$address2', city = '$city', state ='$state', zipcode = $zipcode, username = '$_SESSION['username']'
-                    WHERE username = '$username'";
-                }
-                else{
-                    $sql = "INSERT INTO clientInformation (client_name, address1, address2, city, state, zipcode)
-                    VALUES ('$name', '$adddress1', '$address2', '$city', '$state', $zipcode)";
-                }
-            }
-            
-            $url = "home.html";
-            header("Location: $url");
-        }
-        public function isCreated($username, $db){
+        public function isCreated($username, $db):bool{
             if($username){
                 $sql = "SELECT * FROM clientInformation WHERE username = '$username'";
                 if(mysqli_query($db,$sql)){
@@ -53,8 +13,70 @@
                         return true;
                     }
                 }
+                else{
+					die('no username');
+				}
+            }
+            else{
+                die('no username');
             }
         }
+        public function profile($name = NULL, $address1 = NULL, $address2 = NULL, $city = NULL, $state = NULL, $zipcode = NULL){
+            $db = mysqli_connect('localhost', 'root', 'your_password', 'fuel_thing');
+            if(!$db){
+                die('Error connectinf to the sql server' . mysqli_connect_error());
+            }
+            if($name == NULL && ctype_alpha($_POST['name']) && strlen($_POST['name']) <= 50){
+                $name = $_POST['name'];
+            }
+            else{die('name');}
+            if($address1 == NULL && strlen($_POST['address1']) <= 100){
+                $address1 = $_POST['address1'];
+            }
+            else{die('address1');}
+            if($address2 == NULL && isset($_POST['address2']) && strlen($_POST['address2']) <= 100){
+                $address2 = $_POST['address2'];
+            }
+            if($city == NULL && ctype_alpha($_POST['city']) && strlen($_POST['city']) <= 100){
+                $city = $_POST['city'];
+            }
+            else{die('city');}
+            if($state == NULL && ctype_alpha($_POST['state'])){
+                $state = $_POST['state'];
+            }
+            else{die($_POST['state']);}
+            $pattern = '/\d{5,9}/';
+            if($zipcode == NULL && preg_match($pattern,$_POST['zipcode'])){
+                $zipcode = $_POST['zipcode'];
+            }
+            else{die('zipcode');}
+            if($name && $address1 && $city && $state && $zipcode){
+                $uname = $_SESSION['username'];
+                if($this->isCreated($uname, $db)){
+                    
+                    $sql = "UPDATE clientInformation 
+                    SET client_name = '$name', address1 = '$address1', address2 = '$address2', city = '$city', state ='$state', zipcode = $zipcode, username = '$uname'
+                    WHERE username = '$uname'";
+                    mysqli_query($db,$sql);
+                    $url = "home.html";
+                    header("Location: $url");
+                }
+                else{
+                    $sql = "INSERT INTO clientInformation (client_name, address1, address2, city, state, zipcode)
+                    VALUES ('$name', '$address1', '$address2', '$city', '$state', $zipcode)";
+                    if(!mysqli_query($db,$sql))
+                    {
+                        die("Error description: " . $db -> error);
+                    }
+                    
+                    $url = "home.html";
+                    header("Location: $url");
+                }
+            }
+            
+            
+        }
+        
     }
 
        
