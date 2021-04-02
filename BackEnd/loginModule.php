e793dcae6e8bfcbf79ccc7e9264cbc36bec69552
adalrymple <75194086+adalrymple@users.noreply.github.com>
2021-04-02 01:17:00 -0500

    Add files via upload

--- BackEnd/loginModule.php
+++ BackEnd/loginModule.php
@@ -3,34 +3,71 @@
 session_start();
 class loginMod{
 	public function  login($username = null, $password = null){
+		$db = mysqli_connect('localhost','root','your_password','fuel_thing');
+				if(!$db)	
+				{
+					die('Error Connecting to Sql Server' . mysqli_connect_error());
+				}
 		if ($username == null){
 			$username = $_POST['username'];
 		}
 		if ($password == null){
 			$password = $_POST['password'];
 		}
-		if ($this->validUsername($username) && $this->correctCredentials($username,$password)){
+		if ($this->validUsername($username,$db) && $this->correctCredentials($username,$password,$db)){
 			$_SESSION['username'] = $username;
-			$url = '//localhost/home.html';
+			$sql = "SELECT * FROM ClientInformation WHERE username = '$username'";
+			if(mysqli_query($db,$sql)){
+				$result = mysqli_query($db,$sql);
+				if($result->num_rows == 0){
+					$url = '//localhost/editProfile.html';
+				}
+				else{
+					$url = '//localhost/home.html';
+				}
+			}
+			else
+			{
+				die('Query Failed');
+			}
 			header("Location: $url");
 		}
 		else {
 			echo "<script>alert('Wrong credentials try again')</script>";//Invalid Login
 		}
 	}
-	public function validUsername($username):  bool {
-		$userinfo = array(
-                'username1'=>'password1',
-                'username2'=>'password2'
-                );
-		return (array_key_exists($username,$userinfo));
+	public function validUsername($username,$db):  bool {
+		
+		$sql = "SELECT * FROM UserCredentials WHERE id = '$username'";
+		if(mysqli_query($db,$sql)){
+			$result = mysqli_query($db,$sql);
+			if($result->num_rows == 0){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+		else
+		{
+			die("query failed");
+		}
 	}
-	public function correctCredentials($username, $password): bool {
-		$userinfo = array(
-                'username1'=>'password1',
-                'username2'=>'password2'
-                );
-		return ($userinfo[$username] == $password);
+	public function correctCredentials($username, $password,$db): bool {
+		$sql = "SELECT * FROM UserCredentials WHERE id = '$username' and pswd = '$password'";
+		if(mysqli_query($db,$sql)){
+			$result = mysqli_query($db,$sql);
+			if($result->num_rows == 0){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+		else
+		{
+			die("query failed");
+		}
 	}
 }	
 ?>
--- BackEnd/registration.php
+++ BackEnd/registration.php
@@ -1,21 +1,73 @@
 <?php
     session_start();
-	$db = new mysqli_connect('localhost','root','root','fuel_thing')
-	or die('Error Connecting to Sql Server');
-	
-	
-    if(isset($_POST['username'])&&isset($_POST['password']))
-    {    
-        if( $_POST["username"] && $_POST["password"] )
-        {
-			$sql = "Insert INTO UserCredentials(id,pswd) 
-			VALUES ($username,$password)";
-			if ($db->query($sql) == TRUE){
-				$_SESSION["username"] = $_POST["username"];
-				$url = "loginPage.html";
-				header("Location: $url");
+	class registration
+	{
+			public function uniqueUsername($username,$db)
+			{
+				if($username)
+				{
+					$sql = "SELECT * FROM UserCredentials WHERE id = '$username'";
+					if(mysqli_query($db,$sql))
+					{
+						$result = mysqli_query($db,$sql);
+						if($result->num_rows==0)
+						{
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+					else{
+						die("query failed");
+					}
+				}
+				else{
+					die('no username');
+				}
 			}
-             
-        }
-    }
+			public function register($username = NULL, $password = NULL)
+			{
+				$db = mysqli_connect('localhost','root','your_password','fuel_thing');
+				if(!$db)	
+				{
+					die('Error Connecting to Sql Server' . mysqli_connect_error());
+				}
+				if($username==NULL)
+				{
+					$username = $_POST['username'];
+				}
+				if($password==NULL)
+				{
+					$password = $_POST['password'];
+				}
+				if( $username && $password )
+				{
+					if($this->uniqueUsername($username,$db))
+					{
+						$sql = "INSERT INTO UserCredentials(id,pswd) 
+						VALUES ('$username','$password')";
+						if (mysqli_query($db,$sql))
+						{
+							$_SESSION["username"] = $_POST["username"];
+							$url = "loginPage.html";
+							header("Location: $url");
+						}
+						else
+						{
+							die($password);
+						}
+					}
+					else
+					{
+						die('username already taken');
+					}
+					
+				}
+				mysqli_close($db);
+			}
+			
+	}
+	
 ?>
