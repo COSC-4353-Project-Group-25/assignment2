4715421202f7a4a379bd2b3a129ee4929b9034d0
Kwcunnin <44002642+Kwcunnin@users.noreply.github.com>
2021-04-01 21:11:48 -0500

    Update registration.php

--- BackEnd/registration.php
+++ BackEnd/registration.php
@@ -1,14 +1,21 @@
 <?php
     session_start();
+	$db = new mysqli_connect('localhost','root','root','fuel_thing')
+	or die('Error Connecting to Sql Server');
+	
+	
     if(isset($_POST['username'])&&isset($_POST['password']))
     {    
         if( $_POST["username"] && $_POST["password"] )
         {
-           
-             $_SESSION["username"] = $_POST["username"];
+			$sql = "Insert INTO UserCredentials(id,pswd) 
+			VALUES ($username,$password)";
+			if ($db->query($sql) == TRUE){
+				$_SESSION["username"] = $_POST["username"];
+				$url = "loginPage.html";
+				header("Location: $url");
+			}
+             
         }
-        $url = "loginPage.html";
-        header("Location: $url");
-        
     }
 ?>
